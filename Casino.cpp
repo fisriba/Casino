9f136f498137a012517080b5228f6663d7b27a7d
fisriba <andy311006@gmail.com>
2023-06-11 18:42:20 +0200

    utrka konja pred kraju
    utrka konja pred kraju

--- Casino.cpp
+++ Casino.cpp
@@ -7,41 +7,52 @@ using namespace std;
 int main()
 {
     fstream datoteka;
-        datoteka.open("Povijest.txt", ios::app);
-    char c, polje[3][8] = { {'A','_', '_', '_', '_', '_', '_', 'g'},{'B','_', '_', '_', '_', '_','_','f'},{'C','_', '_', '_', '_','_','_', 'k'} };
-    int x1 = 0, y1 = 0, x2 = 1, y2 = 0, x3 = 2, y3 = 0, z, g;
+    datoteka.open("Povijest.txt", ios::app);
+
+    char c, g;
+    int z, t;
+    char polje[3][8] = { {'A','_', '_', '_', '_', '_', '_', 'g'},
+                         {'B','_', '_', '_', '_', '_','_','f'},
+                         {'C','_', '_', '_', '_','_','_', 'k'} };
+
     cout << "Dobrodosli u utrke konja, unesite svoje ime: ";
-        cin >> g;
-        cout << "Dobrodošli " << g;
-    cout <<"odaberite konja na kojega se kladite : \n                                                    A)\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t                                                    B)\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t                                                    C)\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    ";
-    cin.ignore();
-    for (int i = 1; i < 2; i++)
+    cin >> g;
+    system("cls");
+    cout << "Dobrodosli " << g << "\n";
+
+    cout << "Odaberite konja na kojega se kladite : \n"
+        << "                                                   A)\n"
+        << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t                                                    B)\n"
+        << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t                                                    C)\n\n"
+        << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    ";
+
+    cin >> c;
+    while (c != 'A' && c != 'B' && c != 'C')
     {
+        cout << "Unesite valjanog konja: ";
         cin >> c;
-        if (c == 'A')
-            break;
-        else if (c == 'B')
-            break;
-        else if (c == 'C')
-            break;
-        else
-            cout << "\nUnesite valjanog konja: ";
     }
+
     cout << "\n";
     cout << "Napisi koliko ulazes: ";
     cin >> z;
+
+    int x1 = 0, y1 = 0, x2 = 1, y2 = 0, x3 = 2, y3 = 0;
+
     while ((polje[0][7] == 'g') && (polje[1][7] == 'f') && (polje[2][7] == 'k'))
     {
         int nasumicno;
         Sleep(700);
         srand(time(NULL));
         nasumicno = rand() % 10;
+
         if (nasumicno > 4)
         {
             polje[x1][y1] = '_';
             polje[x1][y1 + 1] = 'A';
             y1++;
         }
+
         system("cls");
         for (int i = 0; i < 3; i++)
         {
@@ -49,15 +60,18 @@ int main()
                 cout << polje[i][j];
             cout << "\n";
         }
+
         Sleep(700);
         srand(time(NULL));
         nasumicno = rand() % 10;
+
         if (nasumicno > 4)
         {
             polje[x2][y2] = '_';
             polje[x2][y2 + 1] = 'B';
             y2++;
         }
+
         system("cls");
         for (int i = 0; i < 3; i++)
         {
@@ -65,15 +79,18 @@ int main()
                 cout << polje[i][j];
             cout << "\n";
         }
+
         Sleep(700);
         srand(time(NULL));
         nasumicno = rand() % 10;
+
         if (nasumicno > 4)
         {
             polje[x3][y3] = '_';
             polje[x3][y3 + 1] = 'C';
             y3++;
         }
+
         system("cls");
         for (int i = 0; i < 3; i++)
         {
@@ -82,48 +99,47 @@ int main()
             cout << "\n";
         }
     }
+
     cout << "\n";
 
     if (polje[0][7] == 'A' && c == 'A')
     {
-        z = z * 100000;
-        cout << "Cestitamo! Vas konj je pobijedio.";
-        cout << "\n";
-        cout << "Vas dobitak je: " << z;
-        goto kraj;
+        t = z * 100000;
+        cout << "Cestitamo! Vas konj je pobijedio.\n";
+        cout << "Vas dobitak je: " << t << "\n";
+        datoteka << g << " je uložio " << z << " i osvojio " << t << " s konjem A.\n";
     }
-    if (polje[0][7] == 'A' && c != 'A')
+    else if (polje[0][7] == 'A' && c != 'A')
     {
-        cout << "Izgubili ste, vise srece drugi put";
-        goto kraj;
+        cout << "Izgubili ste, vise srece drugi put\n";
+        datoteka << g << " je uložio " << z << " i izgubio s konjem A.\n";
     }
-    if (polje[1][7] == 'B' && c == 'B')
+    else if (polje[1][7] == 'B' && c == 'B')
     {
-        z = z * 100000;
-        cout << "Cestitamo! Vas konj je pobijedio.";
-        cout << "\n";
-        cout << "Vas dobitak je: " << z;
-        goto kraj;
+        t = z * 100000;
+        cout << "Cestitamo! Vas konj je pobijedio.\n";
+        cout << "Vas dobitak je: " << t << "\n";
+        datoteka << g << " je uložio " << z << " i osvojio " << t << " s konjem B.\n";
     }
-    if (polje[1][7] == 'B' && c != 'B')
+    else if (polje[1][7] == 'B' && c != 'B')
     {
-        cout << "Izgubili ste, vise srece drugi put";
-        goto kraj;
+        cout << "Izgubili ste, vise srece drugi put\n";
+        datoteka << g << " je uložio " << z << " i izgubio s konjem B.\n";
     }
-    if (polje[2][7] == 'C' && c == 'C')
+    else if (polje[2][7] == 'C' && c == 'C')
     {
-        z = z * 100000;
-        cout << "Cestitamo! Vas konj je pobijedio.";
-        cout << "\n";
-        cout << "Vas dobitak je: " << z;
-        goto kraj;
+        t = z * 100000;
+        cout << "Cestitamo! Vas konj je pobijedio.\n";
+        cout << "Vas dobitak je: " << t << "\n";
+        datoteka << g << " je uložio " << z << " i osvojio " << t << " s konjem C.\n";
     }
-    if (polje[2][7] == 'C' && c != 'C')
+    else if (polje[2][7] == 'C' && c != 'C')
     {
-        cout << "Izgubili ste, vi�e srece drugi put";
-        goto kraj;
+        cout << "Izgubili ste, vise srece drugi put\n";
+        datoteka << g << " je uložio " << z << " i izgubio s konjem C.\n";
     }
 
-kraj:
+    datoteka.close();
+
     return 0;
 }
